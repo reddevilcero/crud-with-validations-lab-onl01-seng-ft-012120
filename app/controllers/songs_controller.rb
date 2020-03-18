77f0ca4db2fb90890676931b9d3e2ecc094a1e8a
reddevilcero <reddevil_cero@hotmail.com>
2020-03-18 15:43:13 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,55 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+  
+  def new
+    @song = Song.new
+  end
+  
+  def edit
+    @song = Song.find(params[:id])
+  end
+  
+  
+  def show
+    @song = Song.find(params[:id])
+  end
+  
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render 'new'
+    end
+  end
+  def update
+    @song = Song.find(params[:id])
+      if @song.update_attributes(song_params)
+        redirect_to @song
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    if @song.destroy
+      redirect_to songs_url
+    else
+      redirect_to songs_url
+    end
+  end
+  
+  
+  
+  private
+
+  def song_params
+    params.require(:song).permit!
+  end
+
+
+
+end
